b353326105fcbbfc90b9b81ba920830fc0e28ad0
kia <kia@csie.io>
2018-08-05 18:15:45 +0800

    fixed ranking align bugs

--- JS/index.js
+++ JS/index.js
@@ -24,7 +24,7 @@ function show_stage(data) {
         data.forEach((o)=>{
           html +=`
           <div class="opt">
-            <i class="fas fa-child ${o.in>0?"":"hidden"}"></i>
+            <i class="fas fa-child fa-lg fa-fw ${o.in>0?"":"hidden"}"></i>
             <div class="name">${o.name}</div>
           </div>
           `
